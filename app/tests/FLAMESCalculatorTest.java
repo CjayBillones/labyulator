2697691a36b0dcb8a16cb86f0769aa420442f333
Alwin de Leon <bludeleon@gmail.com>
2016-05-27 03:16:47 +0800

    add FLAMESCalculator Tests

---
+++ app/tests/FLAMESCalculatorTest.java
@@ -0,0 +1,45 @@
+package app.tests;
+
+import junit.extensions.TestSetup;
+import junit.framework.TestCase;
+
+import app.src.calculator.*;
+
+public class FLAMESCalculatorTest extends TestCase{
+
+  FLAMESCalculator calculator;
+
+  public void setUp(){
+    calculator = new FLAMESCalculator();
+  }
+
+  public void testOption1IsFRIENDSHIP(){
+    boolean result =  calculator.calculateCompatibility("abcd","asd").equals("FRIENDSHIP");
+    assertTrue(result);
+  }
+
+  public void testOption2IsLOVE(){
+    boolean result =  calculator.calculateCompatibility("MATT","DENISE").equals("LOVE");
+    assertTrue(result);
+  }
+
+  public void testOption3IsAFFECTION(){
+    boolean result =  calculator.calculateCompatibility("gwen","cjay").equals("AFFECTION");
+    assertTrue(result);
+  }
+
+  public void testOption4IsMARRIAGE(){
+    boolean result =  calculator.calculateCompatibility("SHILPA","AMIR").equals("MARRIAGE");
+    assertTrue(result);
+  }
+
+  public void testOption5IsENEMY(){
+    boolean result =  calculator.calculateCompatibility("Cjay Billones","Alwin de Leon").equals("ENEMY");
+    assertTrue(result);
+  }
+
+  public void testOption6IsSIBLING(){
+    boolean result =  calculator.calculateCompatibility("putangina walang sibling","yehey").equals("SIBLING");
+    assertTrue(result);
+  }
+}
--- run-test.sh
+++ run-test.sh
@@ -2,3 +2,8 @@ echo "Running tests for InputValidator. . ."
 javac -cp /usr/share/java/junit.jar app/src/util/InputValidator.java app/tests/InputValidatorTest.java
 java -cp /usr/share/java/junit.jar:. junit.textui.TestRunner app.tests.InputValidatorTest
 echo "InputValidator tests done. . ."
+
+echo "Running tests for FLAMESCalculator. . ."
+javac -cp /usr/share/java/junit.jar app/src/calculator/FLAMESCalculator.java app/src/calculator/LoveCalculator.java app/tests/FLAMESCalculatorTest.java
+java -cp /usr/share/java/junit.jar:. junit.textui.TestRunner app.tests.FLAMESCalculatorTest
+echo "FLAMESCalculator tests done. . ."
