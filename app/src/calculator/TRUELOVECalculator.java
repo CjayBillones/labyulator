5a9dc0ea6df1e596d3f518801d52a042570ae3bd
Cjay Billones <billonesciprian08@gmail.com>
2016-05-27 04:09:08 +0800

    Refactor TRUELOVECalculator

--- app/src/calculator/TRUELOVECalculator.java
+++ app/src/calculator/TRUELOVECalculator.java
@@ -3,36 +3,23 @@ package app.src.calculator;
 public class TRUELOVECalculator implements LoveCalculator{
 
   public String calculateCompatibility(String name1, String name2){
-    char[] trueArr ={'t', 'r', 'u', 'e' };
-    char[] loveArr ={'l', 'o', 'v', 'e' };
-    int trueSum = TRUELOVECalculator.getRepetitions(trueArr, name1, name2);
-    int loveSum = TRUELOVECalculator.getRepetitions(loveArr, name1, name2);
-    String result = Integer.toString(trueSum) + Integer.toString(loveSum);
+    int trueSum = count(name1.toLowerCase(), "true") + count(name2.toLowerCase(), "true");
+    int loveSum = count(name1.toLowerCase(), "love") + count(name2.toLowerCase(), "love");
+    String result = (trueSum == 0 && loveSum == 0) ? "0" : Integer.toString(trueSum) + Integer.toString(loveSum);
 
     return result;
   }
 
-  public static int getRepetitions( char[] arr, String name1, String name2){
-    name1 = name1.toLowerCase();
-    name2 = name2.toLowerCase();
-    int sum = 0;
-    int i ,j;
-    for( i = 0; i < name1.length(); i++){
-        for( j = 0 ; j < arr.length ; j++){
-            if(name1.charAt(i) == arr[j]){
-                sum++;
-                break;
-            }
+  private int count(String name, String word){
+    int count = 0;
+    for(int i = 0; i < name.length(); i++){
+      for(int j = 0; j < word.length(); j++){
+        if(name.charAt(i) == word.charAt(j)){
+          count++;
+          break;
         }
+      }
     }
-    for( i = 0; i < name2.length(); i++){
-        for( j = 0 ; j < arr.length ; j++){
-            if(name2.charAt(i) == arr[j]){
-                sum++;
-                break;
-            }
-        }
-    }
-    return sum;
-   }
+    return count;
+  }
 }
