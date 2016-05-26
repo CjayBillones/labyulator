d24da7331d7d144db7c909ae8a7bb7551ef89956
Alwin de Leon <bludeleon@gmail.com>
2016-05-27 03:33:26 +0800

    add TRUELOVECalculator tests

--- app/tests/TRUELOVECalculatorTest.java
+++ app/tests/TRUELOVECalculatorTest.java
@@ -3,44 +3,28 @@ package app.tests;
 import junit.extensions.TestSetup;
 import junit.framework.TestCase;
 
-import app.src.util.*;
+import app.src.calculator.*;
 
-public class InputValidatorTest extends TestCase{
+public class TRUELOVECalculatorTest extends TestCase{
 
-  InputValidator validator;
+  TRUELOVECalculator calculator;
 
   public void setUp(){
-    validator = InputValidator.getInstance();
+    calculator = new TRUELOVECalculator();
   }
 
-  public void testOption1IsValid(){
-    int option = 1;
-    boolean result = validator.validateOption(1);
+  public void testOption1(){
+    boolean result = calculator.calculateCompatibility("Tom Thompson","Shelly Smith").equals("46");
     assertTrue(result);
   }
 
-  public void testOption2IsValid(){
-    int option = 2;
-    boolean result = validator.validateOption(2);
+  public void testOption2(){
+    boolean result = calculator.calculateCompatibility("MATT","DENISE").equals("42");
     assertTrue(result);
   }
 
-  public void testOption3IsValid(){
-    int option = 3;
-    boolean result = validator.validateOption(option);
+  public void testOption3(){
+    boolean result = calculator.calculateCompatibility("SHILPA","AMIR").equals("11");
     assertTrue(result);
   }
-
-  public void testOptionLessThan1IsInvalid(){
-    int option = -1;
-    boolean result = validator.validateOption(option);
-    assertFalse(result);
-  }
-
-  public void testOptionGreaterThan3IsInvalid(){
-    int option = 4;
-    boolean result = validator.validateOption(option);
-    assertFalse(result);
-  }  
-
 }
--- run-test.sh
+++ run-test.sh
@@ -7,3 +7,8 @@ echo "Running tests for FLAMESCalculator. . ."
 javac -cp /usr/share/java/junit.jar app/src/calculator/FLAMESCalculator.java app/src/calculator/LoveCalculator.java app/tests/FLAMESCalculatorTest.java
 java -cp /usr/share/java/junit.jar:. junit.textui.TestRunner app.tests.FLAMESCalculatorTest
 echo "FLAMESCalculator tests done. . ."
+
+echo "Running tests for TRUELOVECalculator. . ."
+javac -cp /usr/share/java/junit.jar app/src/calculator/TRUELOVECalculator.java app/src/calculator/LoveCalculator.java app/tests/TRUELOVECalculatorTest.java
+java -cp /usr/share/java/junit.jar:. junit.textui.TestRunner app.tests.TRUELOVECalculatorTest
+echo "TRUELOVECalculator tests done. . ."
