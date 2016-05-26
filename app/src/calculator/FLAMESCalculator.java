314b8fd2f800e37cbf80cfdff1c72b78359dd2dd
Alwin de Leon <bludeleon@gmail.com>
2016-05-27 02:51:14 +0800

    add TRUELOVE and FLAMES logic

--- app/src/calculator/FLAMESCalculator.java
+++ app/src/calculator/FLAMESCalculator.java
@@ -3,8 +3,95 @@ package app.src.calculator;
 public class FLAMESCalculator implements LoveCalculator{
 
   public String calculateCompatibility(String name1, String name2){
-    // Insert algorithm of FLAMES
-    return "result FLAMES"; // Replace with result variable
+    name1 = name1.toLowerCase();
+    name2 = name2.toLowerCase();
+    String name1_split[] = name1.split(" ");
+    String name2_split[] = name2.split(" ");
+    name1="";
+    name2="";
+    for(int i=0;i<name1_split.length;i++)
+    {
+        name1 = name1+name1_split[i];
+    }
+    for(int i=0;i<name2_split.length;i++)
+    {
+        name2 = name2+name2_split[i];
+    }
+    int length = name1.length()+name2.length();
+    boolean name_check[] = new boolean[name2.length()];
+    for(int i=0;i<name2.length();i++)
+    {
+        name_check[i]=false;
+    }
+    for(int i=0;i<name1.length();i++)
+    {
+        for(int j=0;j<name2.length();j++)
+        {
+            if((name_check[j]==false) && (name1.charAt(i) == name2.charAt(j)))
+            {
+                name_check[j]=true;
+                length = length-2;
+                break;
+            }
+        }
+    }
+    boolean flames_check[] = new boolean[6];
+    for(int i=0;i<6;i++)
+    {
+        flames_check[i] = true;
+    }
+    int count = 6;
+    int k=1,deleted_letters=0;
+    int j;
+    for(j=0;j<=count;j++)
+    {
+        if(k <= length)
+        {
+            if(j == count)
+            {
+                j=0;
+            }
+            if(flames_check[j] == true)
+            {
+                k = k+1;
+            }
+        }
+        if((k-1)==length)
+        {
+            deleted_letters = deleted_letters+1;
+            flames_check[j] = false;
+            k = 1;
+        }
+        if(deleted_letters==6)
+        {
+            if(j==0)
+            {
+                return "FRIENDSHIP";
+            }
+            else if(j==1)
+            {
+                return "LOVE";
+            }
+            else if(j==2)
+            {
+                return "AFFECTION";
+            }
+            else if(j==3)
+            {
+                return "MARRIAGE";
+            }
+            else if(j==4)
+            {
+                return "ENEMY";
+            }
+            else if(j==5)
+            {
+                return "SIBLING";
+            }
+            break;
+        }
+    }
+    return "LABYU";
   }
 
 }
--- app/src/calculator/TRUELOVECalculator.java
+++ app/src/calculator/TRUELOVECalculator.java
@@ -3,8 +3,36 @@ package app.src.calculator;
 public class TRUELOVECalculator implements LoveCalculator{
 
   public String calculateCompatibility(String name1, String name2){
-    // Insert algorithm of True Love
-    return "result TRUE LOVE"; // Replace with result variable 
+    char[] trueArr ={'t', 'r', 'u', 'e' };
+    char[] loveArr ={'l', 'o', 'v', 'e' };
+    int trueSum = TRUELOVECalculator.getRepetitions(trueArr, name1, name2);
+    int loveSum = TRUELOVECalculator.getRepetitions(loveArr, name1, name2);
+    String result = Integer.toString(trueSum) + Integer.toString(loveSum);
+
+    return result;
   }
 
+  public static int getRepetitions( char[] arr, String name1, String name2){
+    name1 = name1.toLowerCase();
+    name2 = name2.toLowerCase();
+    int sum = 0;
+    int i ,j;
+    for( i = 0; i < name1.length(); i++){
+        for( j = 0 ; j < arr.length ; j++){
+            if(name1.charAt(i) == arr[j]){
+                sum++;
+                break;
+            }
+        }
+    }
+    for( i = 0; i < name1.length(); i++){
+        for( j = 0 ; j < arr.length ; j++){
+            if(name2.charAt(i) == arr[j]){
+                sum++;
+                break;
+            }
+        }
+    }
+    return sum;
+   }
 }
