934657ce9068adac89e3e245621e37ee07b5ca68
Cjay Billones <billonesciprian08@gmail.com>
2016-05-27 04:34:05 +0800

    Modify TRUELOVECalculatorTest

--- app/tests/TRUELOVECalculatorTest.java
+++ app/tests/TRUELOVECalculatorTest.java
@@ -13,18 +13,32 @@ public class TRUELOVECalculatorTest extends TestCase{
     calculator = new TRUELOVECalculator();
   }
 
-  public void testOption1(){
-    boolean result = calculator.calculateCompatibility("Tom Thompson","Shelly Smith").equals("46");
-    assertTrue(result);
+  public void testCorrectInput(){
+    String name1 = "Tom Thompson";
+    String name2 = "Shelly Smith";
+    String result = calculator.calculateCompatibility(name1, name2);
+    assertEquals("46", result);
   }
 
-  public void testOption2(){
-    boolean result = calculator.calculateCompatibility("MATT","DENISE").equals("42");
-    assertTrue(result);
+  public void testZeroPercentage(){
+    String name1 = "max";
+    String name2 = "aga";
+    String result = calculator.calculateCompatibility(name1, name2);
+    assertEquals("0", result);
   }
 
-  public void testOption3(){
-    boolean result = calculator.calculateCompatibility("SHILPA","AMIR").equals("11");
-    assertTrue(result);
+  public void testOver100Percent(){
+    String name1 = "Hoooooodoooooor";
+    String name2 = "Bran Stark";
+    String result = calculator.calculateCompatibility(name1, name2);
+    assertEquals("412", result);
   }
+
+  public void testOver1000Percent(){
+    String name1 = "Hoooodoooooorrrrrrrrrrr";
+    String name2 = "Hoooold theee dooooooor";
+    String result = calculator.calculateCompatibility(name1, name2);
+    assertEquals("1625", result);
+  }
+
 }
